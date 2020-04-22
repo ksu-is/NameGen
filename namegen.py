93346c62f6310372ed68182965a559bfd6277161
Matthew Intriago <59572682+matthewintriago@users.noreply.github.com>
2020-04-22 10:57:39 -0400

    Create namegen.py

---
+++ namegen.py
@@ -0,0 +1,24 @@
+import requests
+import random
+
+
+
+response = requests.get(word_site)
+WORDS = response.content.splitlines()
+
+
+
+
+
+
+
+def Name():
+	global the_name
+	while True:
+		gender=input("Is your baby a boy or girl? :")
+		if gender = boy
+			import boynames.txt
+		if gender=girl:
+			import girlnames.txt
+		else:
+			print("Please state if the baby is a boy or girl.")
