2ac8a804ac067935dd8d14ed9f9a3cdea446d08d
Matthew Intriago <59572682+matthewintriago@users.noreply.github.com>
2020-04-27 20:59:54 -0400

    Update namegen.py

--- namegen.py
+++ namegen.py
@@ -6,45 +6,51 @@ word_site = "https://svnweb.freebsd.org/csrg/share/dict/propernames?view=co"
 response = requests.get(word_site)
 WORDS = response.content.splitlines()
 
-the_name=''
+
 the_gender=''
+the_name=''
 suggestions=''
 number=0
 
-def gen():
-	global the_gender
-	while True:
-		baby_gender=input("What is the baby's gender?(boy or girl)")
-		if baby_gender.isalpha():
-			the_gender+=baby_gender
-			break
-		else:
-			print("Please enter either boy or girl!")
-gen()
+
+
+
+def gend():
+   global the_gender
+   while True:
+      your_gen=input("Is the baby a boy or girl?")
+      if your_gen.isalpha():
+         the_gender+=your_gen
+         break
+      else:
+         print("Please try to use letters from the alphabet!")
+gend()
+
 
 def suggest():
-	global the_word
-	global suggestions
-	for word in WORDS:
-		gen=random.choice(WORDS).capitalize()
-		print(gen.decode('utf-8'))
-		like_or=input("Do you like this name? (yes or no)")
-		if like_or=="yes":
-			the_word+=str(gen.decode('utf-8'))
-			break
-		elif like_or=="maybe":
-			global number 
-			suggestions+=str(gen.decode('utf-8'))+"\n"
-			number+=1
-		if number==5:
-			print(suggestions,"\n")
-			would_you=input("Which one of these names do you like?")
-			if would_you in suggestions:
-				the_word+=would_you
-				break
-			else: 
-				print("Copy the name exactly as it is above!")
-				
+   global the_name
+   global suggestions
+   for word in WORDS:
+      gen=random.choice(WORDS).capitalize()
+      print(gen.decode('utf-8'))
+      happy_or=input("Do you like the name? (yes or no or maybe)")
+      if happy_or=="yes":
+         the_name+=str(gen.decode('utf-8'))
+         break
+      elif happy_or=="maybe":
+         global number 
+         suggestions+=str(gen.decode('utf-8'))+"\n"
+         number+=1
+      if number==5:
+         print(suggestions,"\n")
+         would_you=input("Which one of the suggestions do you like?")
+         if would_you in suggestions:
+            the_name+=would_you
+            break
+         else:
+            print("Type the word in the suggestions correctly!")
+                
+      
+            
 suggest()
-
-print("Your baby ",the_gender,"'s","name is ",the_name)
+print("Your baby",the_gender,"'s","name will be:",the_name)
